d6636d1e24a5469f8d1d9d3ec100e755d16d86f7
David Andai <andaidavid97@gmail.com>
2025-06-20 21:38:10 +0300

    mistral test

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.routers import project, github, upload
+import os
 
 app = FastAPI()
 
